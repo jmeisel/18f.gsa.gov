53b45e9b2ea493bb3d546f87354f33626b41ef0a
Greg Boone <gregory.boone@gsa.gov>
2014-11-12 13:11:45 -0500

    Fixes map.js to match new data structure

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,29 +1,12 @@
 var teamAPI = "/api/data/team.json";
-var locationsAPI = "/api/data/locations.json";
 $(document).ready( function() {
   var locations = $.ajax({
     dataType: "json",
-    url: locationsAPI,
-  }).done(function(data) {
-    return data;
-  });
-  $.ajax({
-    dataType: "json",
     url: teamAPI,
   }).done(function(data) {
-    locations = $.each(locations.responseJSON, function( index, object ) {
-      locations.responseJSON[index].members = [];
-      return
-    });
-    $.each(data, function( index, object ) {
-      if (object.location && object.hidden != true) {
-        locations[object.location].members.push([object.name, object.full_name]);
-      }
-      return locations;
-    });
-    $.each(locations, function( index, object ) {
+    $.each(data['locations'], function( index, object ) {
       popup = L.popup([object.lat, object.long] );
-      popupContent = "<a href='#"+index+"'>"+object.city+", "+object.state+"</a>";
+      popupContent = "See members of our team in: <a href='#"+index+"'>"+object.city+", "+object.state+"</a>.";
       L.marker([object.lat, object.long]).bindPopup(
         popupContent, 'p',popup).addTo(map);
     })
